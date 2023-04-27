29139c7ffbcd2fcc3c509c307619bc0e243490b3
Guicod3 <guilmiranda50@gmail.com>
2023-04-26 22:22:43 -0300

    melhorias com js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += Letra;
-        }, 75 * i);
-    });
+function escrevendoLetra(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 75 * i);
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
-ativaLetra(titulo);
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegação-primária')
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+})
